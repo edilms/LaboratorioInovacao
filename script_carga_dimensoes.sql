133672f7bf5fa46b941a659a98f0c16ae06747bf
edilms <prof.edilberto.silva@gmail.com>
2023-11-16 08:49:35 -0300

    Add files via upload
    Atualização demais dimensões

--- script_carga_dimensoes.sql
+++ script_carga_dimensoes.sql
@@ -1,5 +1,9 @@
 -- Cargas Dimensoes FACSENAC
 
+-- ----------------------------
+-- Dimensão Condição Econômica
+-- ----------------------------
+
 use mmd_Facsenac;
 
 -- delete from DimCondicaoEconomica;
@@ -15,5 +19,88 @@ VALUES
 (5, "Classe alta");
 
 Select * from DimCondicaoEconomica;
+
+-- ----------------------------
+-- Dimensão Raça
+-- ----------------------------
+
+use mmd_Facsenac;
+
+-- delete from DimRaca;
+
+INSERT into DimRaca (
+  IddimRaca,
+  DeRaca)
+VALUES
+(1, "Branca"),
+(2, "Preta"),
+(3, "Amarela"),
+(4, "Parda"),
+(5, "Indígena");
+
+Select * from DimRaca;
+
+
+-- ----------------------------
+-- Dimensão Sexo
+-- ----------------------------
+
+use mmd_Facsenac;
+
+-- delete from DimSexo;
+
+INSERT into DimSexo (
+  IddimSexo,
+  DeSexo)
+VALUES
+(1, "Masculino"),
+(2, "Feminino");
+
+Select * from DimSexo;
+
+
+-- ----------------------------
+-- Dimensão Sexo
+-- ----------------------------
+
+use mmd_Facsenac;
+
+-- delete from DimCurso;
+
+INSERT into DimCurso (
+  IddimCurso,
+  DeCurso)
+VALUES
+(1, "ADS"),
+(2, "GTI"),
+(3, "TCD"),
+(4, "TSI");
+
+Select * from DimCurso;
+
+
+-- ----------------------------
+-- Dimensão turno
+-- ----------------------------
+
+use mmd_Facsenac;
+
+-- delete from Dimturno;
+
+INSERT into Dimturno (
+  IddimTurno,
+  DeTurno)
+VALUES
+(1, "Matutino"),
+(2, "Vespertino"),
+(3, "Noturno");
+
+Select * from Dimturno;
+
+
+
+
+
+
 
 
